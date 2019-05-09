400c2c4247ecf6a13dea0a52860e861c440b21bc
zhang <zhang_product@163.com>
2019-05-09 17:39:27 +0800

    Signed-off-by: zhang <zhang_product@163.com>

---
+++ src/main/java/com/demo/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+/**
+ * Title:
+ * Project: DEMOS
+ *
+ * @author LDY
+ * Created on 2019/5/9.
+ * Company: 万达信息股份有限公司
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+    }
+}
